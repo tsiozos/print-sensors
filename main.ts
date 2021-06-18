cbb8e14f61c960bfd32721bd03e7e6066950bd27
tsiozos <38419287+tsiozos@users.noreply.github.com>
2021-06-18 13:35:37 +0300

    Ενημέρωση main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,17 @@
+let timeDelta = 0   //delta of local time in tenths of seconds to network time
+
 input.onButtonPressed(Button.A, function () {
 	basic.showString("" + (getIDencoded()))
     input.calibrateCompass()
 })
-function getIDencoded () {
+function getIDencoded (): string {
     return "" + Math.abs(control.deviceSerialNumber() % 100)
 }
+
+function getIDnum (): number {
+    return Math.abs(control.deviceSerialNumber() % 100)
+}
+
 function getSensorsString () {
 	let idstr = getIDencoded()
     let temp = input.temperature()-3.5
@@ -19,6 +26,18 @@ function getSensorsString () {
 radio.setGroup(1)
 radio.setTransmitPower(7)
 basic.showString("" + (getIDencoded()))
+
+radio.onReceivedValue(function (name: string, value: number) {
+    if (name === "SYNC") {
+        // the network time is nettime = input.runningTime()/100 - timeDelta
+        timeDelta = input.runningTime()/100 - value
+        radio.sendValue("SYNCACK",0)
+    }
+    else if (name === "WAKE") {
+        radio.sendValue("STATION", getIDnum())
+    }
+
+})
 
 basic.forever(function () {
     let sensstr = getSensorsString()
